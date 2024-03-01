ae091b9044a0d6dca469674d3f82fcaea2fb6731
hyi96 <yihao0906@gmail.com>
2024-02-29 21:01:53 -0800

    improved test

--- frontend/src/tests/components/OurTable.test.js
+++ frontend/src/tests/components/OurTable.test.js
@@ -2,75 +2,36 @@ import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import OurTable, { ButtonColumn, DateColumn, PlaintextColumn} from "main/components/OurTable";
 
 describe("OurTable tests", () => {
-    const data = [
+    const threeRows = [
         {
             col1: 'Hello',
-            col2: 'asd',
+            col2: 'World',
             createdAt: '2021-04-01T04:00:00.000',
             log: "foo\nbar\n  baz",
         },
         {
             col1: 'react-table',
-            col2: 'hdhdh',
+            col2: 'rocks',
             createdAt: '2022-01-04T14:00:00.000',
             log: "foo\nbar",
 
         },
         {
             col1: 'whatever',
-            col2: 'jiji',
+            col2: 'you want',
             createdAt: '2023-04-01T23:00:00.000',
             log: "bar\n  baz",
-        },
-        {
-            col1: '3',
-            col2: '4',
-            createdAt: '2023-04-01T23:00:00.000',
-            log: "5",
-        },
-        {
-            col1: '4',
-            col2: '5',
-            createdAt: '2023-04-01T23:00:00.000',
-            log: "6",
-        },
-        {
-            col1: '5',
-            col2: '6',
-            createdAt: '2023-04-01T23:00:00.000',
-            log: "7",
-        },
-        {
-            col1: '6',
-            col2: '7',
-            createdAt: '2023-04-01T23:00:00.000',
-            log: "8",
-        },
-        {
-            col1: '7',
-            col2: '8',
-            createdAt: '2023-04-01T23:00:00.000',
-            log: "9",
-        },
-        {
-            col1: '8',
-            col2: '9',
-            createdAt: '2023-04-01T23:00:00.000',
-            log: "10",
-        },
-        {
-            col1: '9',
-            col2: '10',
-            createdAt: '2023-04-01T23:00:00.000',
-            log: "11",
-        },
-        {
-            col1: '10',
-            col2: '11',
-            createdAt: '2023-04-01T23:00:00.000',
-            log: "12",
         }
     ];
+
+    const manyRows = Array.from({ length: 11 }, (_, index) => ({
+        col1: `${index}`,
+        col2: 'whatever',
+        createdAt: '2023-04-01T23:00:00.000',
+        log: "foo\n",
+    }))
+
+    const pageSize = 10;
 
     const clickMeCallback = jest.fn();
 
@@ -94,15 +55,15 @@ describe("OurTable tests", () => {
         );
     });
 
-    test("renders a table with two rows without crashing", () => {
+    test("renders a table with three rows without crashing", () => {
         render(
-            <OurTable columns={columns} data={data.slice(0,2)} />
+            <OurTable columns={columns} data={threeRows} />
         );
     });
 
     test("The button appears in the table", async () => {
         render(
-            <OurTable columns={columns} data={data} />
+            <OurTable columns={columns} data={threeRows} />
         );
 
         expect(await screen.findByTestId("testId-cell-row-0-col-Click-button")).toBeInTheDocument();
@@ -113,14 +74,14 @@ describe("OurTable tests", () => {
 
     test("default testid is testId", async () => {
         render(
-            <OurTable columns={columns} data={data} />
+            <OurTable columns={columns} data={threeRows} />
         );
         expect(await screen.findByTestId("testid-header-col1")).toBeInTheDocument();
     });
 
     test("click on a header and a sort caret should appear", async () => {
         render(
-            <OurTable columns={columns} data={data} testid={"sampleTestId"} />
+            <OurTable columns={columns} data={threeRows} testid={"sampleTestId"} />
         );
 
         expect(await screen.findByTestId("sampleTestId-header-col1")).toBeInTheDocument();
@@ -139,34 +100,41 @@ describe("OurTable tests", () => {
         expect(await screen.findByText("🔽")).toBeInTheDocument();
     });
 
-    test("pagination ui should not be visible when the number of rows of data is less than or equal to the default page size (10)", async () => {
+    test("pagination ui should not be visible when the number of rows of data is less than or equal to the default page size", async () => {
         render(
-            <OurTable columns={columns} data={data.slice(0,10)} testid={"sampleTestId"}/>
+            <OurTable columns={columns} data={threeRows} testid={"sampleTestId"}/>
         );
-        expect(screen.getByTestId('sampleTestId-cell-row-9-col-col1')).toBeInTheDocument(); //confirm the last row is in the table of 10 rows
+        expect(screen.getByTestId(`sampleTestId-cell-row-${threeRows.length-1}-col-col1`)).toBeInTheDocument(); //confirm the last row is in the table
+        expect(screen.queryByTestId("pagination-ui")).not.toBeInTheDocument(); 
+
+        const data = manyRows.slice(0, pageSize);
+        render(
+            <OurTable columns={columns} data={data} testid={"sampleTestId"}/>
+        );
+        expect(screen.getByTestId(`sampleTestId-cell-row-${data.length-1}-col-col1`)).toBeInTheDocument(); //confirm the last row is in the table
         expect(screen.queryByTestId("pagination-ui")).not.toBeInTheDocument(); 
     });
 
-    test("pagination ui should be visible when the number of rows of data is more than the default page size (10)", async () => {
+    test("pagination ui should be visible when the number of rows of data is more than the default page size", async () => {
         render(
-            <OurTable columns={columns} data={data}/>
+            <OurTable columns={columns} data={manyRows}/>
         );
         expect(screen.getByTestId("pagination-ui")).toBeInTheDocument(); 
     });
 
     test("correct data is rendered when next page or previous page buttons is clicked", async () => {
         render(
-            <OurTable columns={columns} data={data} testid={"sampleTestId"}/>
+            <OurTable columns={columns} data={manyRows} testid={"sampleTestId"}/>
         );
         expect(await screen.findByTestId("page-indicator")).toHaveTextContent("Page 1 of 2"); // confirm current page number
-        expect(screen.getByTestId('sampleTestId-cell-row-9-col-col1')).toBeInTheDocument();
+        expect(screen.getByTestId(`sampleTestId-cell-row-${pageSize-1}-col-col1`)).toBeInTheDocument();
         fireEvent.click(screen.getByTestId("goto-next-page-button"));
         expect(await screen.findByTestId("page-indicator")).toHaveTextContent("Page 2 of 2"); 
-        expect(screen.getByTestId('sampleTestId-cell-row-10-col-col1')).toBeInTheDocument(); // confirm element in the next page is displayed
-        expect(screen.queryByTestId('sampleTestId-cell-row-9-col-col1')).not.toBeInTheDocument(); // confirm element from previousd page is not displayed
-        fireEvent.click(screen.getByTestId("goto-previous-page-button"));
+        expect(screen.getByTestId(`sampleTestId-cell-row-${pageSize}-col-col1`)).toBeInTheDocument(); // confirm element in the next page is displayed
+        expect(screen.queryByTestId(`sampleTestId-cell-row-${pageSize-1}-col-col1`)).not.toBeInTheDocument(); // confirm element from previousd page is not displayed
+        fireEvent.click(screen.getByTestId("goto-previous-page-button")); // check go to previous page button
         expect(await screen.findByTestId("page-indicator")).toHaveTextContent("Page 1 of 2"); 
-        expect(screen.queryByTestId('sampleTestId-cell-row-10-col-col1')).not.toBeInTheDocument();
-        expect(screen.getByTestId('sampleTestId-cell-row-9-col-col1')).toBeInTheDocument(); 
+        expect(screen.queryByTestId(`sampleTestId-cell-row-${pageSize}-col-col1`)).not.toBeInTheDocument();
+        expect(screen.getByTestId(`sampleTestId-cell-row-${pageSize-1}-col-col1`)).toBeInTheDocument(); 
     });
 });
