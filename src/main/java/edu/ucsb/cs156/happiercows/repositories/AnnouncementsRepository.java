6d9cfe7fe12a2b7c9bdd74f03ef1283c6acbc651
anguyen412 <anguyen412@ucsb.edu>
2024-03-06 05:24:58 -0800

    AN - added controllers and tests for Announcements

--- src/main/java/edu/ucsb/cs156/happiercows/repositories/AnnouncementsRepository.java
+++ src/main/java/edu/ucsb/cs156/happiercows/repositories/AnnouncementsRepository.java
@@ -2,10 +2,9 @@ package edu.ucsb.cs156.happiercows.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
 import edu.ucsb.cs156.happiercows.entities.Announcements;
 
 @Repository
 public interface AnnouncementsRepository extends CrudRepository<Announcements, Long> {
-    
+    Iterable<Announcements> findAllByCommonsId(Long commonsId);
 }
