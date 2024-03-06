ca9359efe7034a04d8f9b0a46c09a2bd3191699c
Kole Kikuta <ktkikuta@ucsb.edu>
2024-03-05 17:10:43 -0800

    announcements f1 initial commit

---
+++ frontend/src/fixtures/announcementsFixtures.js
@@ -0,0 +1,157 @@
+export const chatMessageFixtures = {
+    oneAnouncement: [
+        {
+            "id": 1,
+            "commonsId": 1,
+            "start": "2024-02-29T23:57:46.576+00:00",
+            "end": "2024-03-01T23:57:46.576+00:00",
+            "announcement": "hello world"
+        }
+    ],
+
+    threeAnouncements: [
+        {
+            "id": 1,
+            "commonsId": 1,
+            "start": "2024-02-29T23:57:46.576+00:00",
+            "end": "2024-03-01T23:57:46.576+00:00",
+            "announcement": "hello world - 1"
+        },
+        {
+            "id": 2,
+            "commonsId": 1,
+            "start": "2024-01-29T23:57:46.576+00:00",
+            "end": "2024-01-30T23:57:46.576+00:00",
+            "announcement": "hello world - 2"
+        },
+        {
+            "id": 2,
+            "commonsId": 1,
+            "start": "2024-05-01T23:57:46.576+00:00",
+            "end": "2024-05-02T23:57:46.576+00:00",
+            "announcement": "hello world - 3"
+        }
+    ],
+    twelveChatMessages: [
+        {
+            "id": 1,
+            "userId": 1,
+            "commonsId": 1,
+            "timestamp": "2023-08-17T23:57:46.576+00:00",
+            "message": "This should not appear",
+            "dm": false,
+            "toUserId": 0,
+            "hidden": false
+        },
+        {
+            "id": 2,
+            "userId": 3,
+            "commonsId": 1,
+            "timestamp": "2023-08-18T02:59:11.125+00:00",
+            "message": "This should also be cut off",
+            "dm": false,
+            "toUserId": 0,
+            "hidden": true
+        },
+        {
+            "id": 3,
+            "userId": 2,
+            "commonsId": 1,
+            "timestamp": "2023-08-18T03:00:28.325+00:00",
+            "message": "Hello World",
+            "dm": false,
+            "toUserId": 0,
+            "hidden": false
+        },
+        {
+            "id": 4,
+            "userId": 1,
+            "commonsId": 1,
+            "timestamp": "2023-08-19T23:57:46.576+00:00",
+            "message": "Crazy that you found me up here",
+            "dm": false,
+            "toUserId": 0,
+            "hidden": false
+        },
+        {
+            "id": 5,
+            "userId": 3,
+            "commonsId": 1,
+            "timestamp": "2023-08-20T02:59:11.125+00:00",
+            "message": "How are you doing???",
+            "dm": false,
+            "toUserId": 0,
+            "hidden": true
+        },
+        {
+            "id": 6,
+            "userId": 2,
+            "commonsId": 1,
+            "timestamp": "2023-08-21T02:59:28.325+00:00",
+            "message": "It's hard work writing all these messages",
+            "dm": false,
+            "toUserId": 0,
+            "hidden": false
+        },
+        {
+            "id": 7,
+            "userId": 1,
+            "commonsId": 1,
+            "timestamp": "2023-08-22T23:57:46.576+00:00",
+            "message": "I'm not too creative about it...",
+            "dm": false,
+            "toUserId": 0,
+            "hidden": false
+        },
+        {
+            "id": 8,
+            "userId": 3,
+            "commonsId": 1,
+            "timestamp": "2023-08-23T02:59:11.125+00:00",
+            "message": "Look symbols!! => (*&$#^)@#*^)*&%^(*!&^)(",
+            "dm": false,
+            "toUserId": 0,
+            "hidden": true
+        },
+        {
+            "id": 9,
+            "userId": 2,
+            "commonsId": 1,
+            "timestamp": "2023-08-24T02:59:28.325+00:00",
+            "message": "Hows the project going?",
+            "dm": false,
+            "toUserId": 0,
+            "hidden": false
+        },
+        {
+            "id": 10,
+            "userId": 1,
+            "commonsId": 1,
+            "timestamp": "2023-08-24T23:57:46.576+00:00",
+            "message": "More chat testing...",
+            "dm": false,
+            "toUserId": 0,
+            "hidden": false
+        },
+        {
+            "id": 11,
+            "userId": 3,
+            "commonsId": 1,
+            "timestamp": "2023-08-25T02:59:11.125+00:00",
+            "message": "This should appear, though",
+            "dm": false,
+            "toUserId": 0,
+            "hidden": true
+        },
+        {
+            "id": 12,
+            "userId": 2,
+            "commonsId": 1,
+            "timestamp": "2023-08-25T02:59:28.325+00:00",
+            "message": "This one too!",
+            "dm": false,
+            "toUserId": 0,
+            "hidden": false
+        },
+    ],
+}
---
+++ frontend/src/main/components/Announcements/announcementsForm.js
@@ -0,0 +1,121 @@
+import { Button, Form, Row, Col } from 'react-bootstrap';
+import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
+
+function announcementsForm({ initialContents, submitAction, buttonLabel = "Create" }) {
+
+    // Stryker disable all
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+    } = useForm(
+        { defaultValues: initialContents || {}, }
+    );
+    // Stryker restore all
+
+    const navigate = useNavigate();
+
+    // For explanation, see: https://stackoverflow.com/questions/3143070/javascript-regex-iso-datetime
+    // Note that even this complex regex may still need some tweaks
+
+    // Stryker disable next-line Regex
+    const isodate_regex = /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d)/i;
+
+    return (
+
+        <Form onSubmit={handleSubmit(submitAction)}>
+
+
+            <Row>
+                {initialContents && (
+                    <Col>
+                        <Form.Group className="mb-3" >
+                            <Form.Label htmlFor="id">Id</Form.Label>
+                            <Form.Control
+                                data-testid="announcementsForm-id"
+                                id="id"
+                                type="text"
+                                {...register("id")}
+                                value={initialContents.id}
+                                disabled
+                            />
+                        </Form.Group>
+                    </Col>
+                )}
+
+                <Col>
+                    <Form.Group className="mb-3" >
+                        <Form.Label htmlFor="start">Start (iso format)</Form.Label>
+                        <Form.Control
+                            data-testid="announcementsForm-start"
+                            id="start"
+                            type="datetime-local"
+                            isInvalid={Boolean(errors.start)}
+                            {...register("start", { required: true, pattern: isodate_regex })}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.start && 'Start is required. '}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Col>
+                <Col>
+                    <Form.Group className="mb-3" >
+                        <Form.Label htmlFor="end">End (iso format)</Form.Label>
+                        <Form.Control
+                            data-testid="announcementsForm-end"
+                            id="end"
+                            type="datetime-local"
+                            isInvalid={Boolean(errors.end)}
+                            {...register("localDateTime", { pattern: isodate_regex })}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.end?.message}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col>
+                    <Form.Group className="mb-3" >
+                        <Form.Label htmlFor="announcement">Announcement</Form.Label>
+                        <Form.Control
+                            data-testid="announcementForm-announcement"
+                            id="announcement"
+                            type="text"
+                            isInvalid={Boolean(errors.announcement)}
+                            {...register("announcement", {
+                                required: "Announcement is required."
+                            })}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.announcement?.message}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col>
+                    <Button
+                        type="submit"
+                        data-testid="announcementForm-submit"
+                    >
+                        {buttonLabel}
+                    </Button>
+                    <Button
+                        variant="Secondary"
+                        onClick={() => navigate(-1)}
+                        data-testid="announcementForm-cancel"
+                    >
+                        Cancel
+                    </Button>
+                </Col>
+            </Row>
+        </Form>
+
+    )
+}
+
+export default announcementsForm;
